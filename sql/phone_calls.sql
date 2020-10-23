3b0163bd593da763e4f0d24c31ca2883cee2c70a
Mohab Fekry <mfekry@google.com>
2020-10-23 12:55:48 +0200

    Add address-related fields to all views to better identify locations having the same 'locationName' value
    Some businesses use the same name for all their locations (usually just their brand name or a couple of variations of it). This, coupled with rolling up all locations to a single location group, makes it impossible to drill-down to (meaningful) location-specific metrics.
    
    Change-Id: I33b4c692e5b77a7fe6f4b31a33150d3597f66e6c
    GitOrigin-RevId: 623db49abc9a4ee86b5a163a7c1b70f925011af7

--- sql/phone_calls.sql
+++ sql/phone_calls.sql
@@ -2,6 +2,11 @@ SELECT
   hc.locationName AS name,
   hc.timeZone AS timeZone,
   l.locationName AS locationName,
+  ARRAY_TO_STRING(l.address.addressLines, ", ") AS addressLines,
+  l.address.locality AS locality,
+  l.address.administrativeArea AS administrativeArea,
+  l.address.postalCode AS postalCode,
+  l.address.regionCode AS regionCode,
   metricValues.metric AS metric,
   metricValues.dimensionalValues AS dimensionalValues
 FROM
