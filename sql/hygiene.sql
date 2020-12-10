c4aa8204ee34a79cd20b342df7934a4d6433e033
Miguel Fernandes <miguelfc@google.com>
2020-12-10 12:24:29 +0100

    Added latlng to schema as per API structure.
    Change-Id: I51173d6cb9be6b087ee2a450d70a13a84ef20318
    GitOrigin-RevId: c9579cce118cec7afb59fb63acacf778afe8602e

--- sql/hygiene.sql
+++ sql/hygiene.sql
@@ -8,7 +8,7 @@ SELECT
   IF(ARRAY_LENGTH(address.addressLines) IS NULL, FALSE, TRUE) AS hasAddress,
   IF(regularHours IS NULL, FALSE, TRUE) AS hasHours,
   IF(websiteUrl IS NULL, FALSE, TRUE) AS hasWebsite,
-  IF(serviceArea.radius.latlng IS NULL, FALSE, TRUE) AS hasLatlong,
+  IF(latlng IS NULL, FALSE, TRUE) AS hasLatlong,
   IF(primaryPhone IS NULL, FALSE, TRUE) AS hasPhone,
   locationState.isDisabled IS TRUE AS isDisabled,
   locationState.isPublished IS TRUE AS isPublished,
